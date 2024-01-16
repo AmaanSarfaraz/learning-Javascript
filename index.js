9bb4b152ab8011abc8da97a18106b62a136c8537
AMAAN SARFARAZ <Sarfarazmohammad369@gmail.com>
2024-01-16 17:29:25 +0530

    getting started

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("sarfaraz");
+let age = "33abc";
+console.log(typeof age);
+console.log(typeof age);
+console.log(typeof NaN);
+
+//   if we convert a string to a number it will be converted but it gives us a return value NaN and if u check the Type of NaN it is number
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 1 = true and 0 = false and empty string is also false
+let isLoggedIn = "sarfaraz";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
