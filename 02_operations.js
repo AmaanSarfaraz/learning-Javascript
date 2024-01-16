887f446d4d0cba85fa77ebe774646569b45bfcf5
AMAAN SARFARAZ <Sarfarazmohammad369@gmail.com>
2024-01-16 17:33:46 +0530

    opertions

---
+++ 02_operations.js
@@ -0,0 +1,9 @@
+let value = 34;
+let negValue = -value;
+console.log(negValue);
+console.log(4 + 2);
+console.log(4 - 2);
+console.log(4 * 2);
+console.log(4 ** 2);
+console.log(4 / 2);
+console.log(4 % 2);
